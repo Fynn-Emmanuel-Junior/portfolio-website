4dd3f4b18bdb4d43dafad0f915df47d92d9dd06b
fynn emmanuel junior <fynn.emmanuel.junior@gmail.com>
2025-08-18 16:20:32 +0000

    updated page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -190,6 +190,7 @@ const projects = [
   { title: 'GoVibe Website', description: 'Next js, Tailwind css,typescript,Framer motion', image: port2,link: 'https://govibe-six.vercel.app/',github: 'https://github.com/GoVibee/landing-page' },
   { title: 'Kodo Scholarships', description: 'React JS,Tailwind CSS,Redux,Node JS,Express JS,Mongo DB,Tawk Chatbot,Digital Ocean', image: port3,link: 'https://kodo-web-gamma.vercel.app/',github: 'https://github.com/Fynn-Emmanuel-Junior/kodo-web'},
   { title: 'Kodo Scholarships Mobile App ', description: 'React Native JS,Expo and EAS,App store connect,Playstore,Expo notifications, Async Storage, Expo Server,Node JS,Express JS,Mongo DB,Digital Ocean', image: port5,link: 'https://apps.apple.com/app/id6745784862',github: 'https://github.com/Kodo-UG/kodo-mobile-app'},
+  { title: 'GoVibe Admin ', description: 'Next js , Tailwind css,typescript', image: port5,link: 'https://govibe-admin.vercel.app/',github: 'https://github.com/GoVibee/admin'},
 ];
 const ProjectsSection = () => (
     <SectionWrapper id="projects" className="bg-white font-plus">
