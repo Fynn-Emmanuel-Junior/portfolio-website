7b2459a8d311de40b6bc35df07587afd46b641d3
fynn emmanuel junior <fynn.emmanuel.junior@gmail.com>
2025-08-18 09:57:55 +0000

    updated pages

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -188,7 +188,7 @@ const SkillsSection = () => (
 const projects = [
   { title: 'Portfolio website', description: 'Next js, EmailJS,Tailwind css,Typescript,Framer motion', image: port1,link: 'https://fynn-dev.vercel.app/',github: 'https://github.com/Fynn-Emmanuel-Junior/portfolio-website'},
   { title: 'GoVibe Website', description: 'Next js, Tailwind css,typescript,Framer motion', image: port2,link: 'https://govibe-six.vercel.app/',github: 'https://github.com/GoVibee/landing-page' },
-  { title: 'Kodo Scholarships', description: 'React JS,Tailwind CSS,Redux,Node JS,Express JS,Mongo DB,Tawk Chatbot,Digital Ocean', image: port3,link: 'https://kodo-web-eta.vercel.app/',github: 'https://github.com/Kodo-UG/kodo-web' },
+  { title: 'Kodo Scholarships', description: 'React JS,Tailwind CSS,Redux,Node JS,Express JS,Mongo DB,Tawk Chatbot,Digital Ocean', image: port3,link: 'https://kodo-web-gamma.vercel.app/',github: 'https://github.com/Fynn-Emmanuel-Junior/kodo-web'},
   { title: 'Kodo Scholarships Mobile App ', description: 'React Native JS,Expo and EAS,App store connect,Playstore,Expo notifications, Async Storage, Expo Server,Node JS,Express JS,Mongo DB,Digital Ocean', image: port5,link: 'https://apps.apple.com/app/id6745784862',github: 'https://github.com/Kodo-UG/kodo-mobile-app'},
 ];
 const ProjectsSection = () => (
