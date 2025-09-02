bf0a5b2ca8d0d29259f69dcd0567af76f67a4724
fynn emmanuel junior <fynn.emmanuel.junior@gmail.com>
2025-09-02 14:57:50 +0000

    updated pages

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -270,16 +270,16 @@ const ProjectsSection = () => {
             <motion.div variants={fadeIn()} className="w-full">
                 <Slider {...sliderSettings}>
                     {projects.map(project => (
-                        <a href={project.link} key={project.title} className="px-2 md:px-4 h-64 w-full">
+                        <a href={project.link} key={project.title} className="px-2 cursor-pointer md:px-4 h-64 w-full">
                             <div className="bg-white w-full text-gray-800 rounded-lg shadow-lg overflow-hidden group border border-gray-700 hover:-translate-y-2 transition-transform duration-300 h-full flex flex-col">
-                                <div className="relative h-56 bg-gray-100 flex items-center justify-center">
+                                <a href={project.link} className="cursor-pointer relative h-56 bg-gray-100 flex items-center justify-center">
                                     <Image 
                                         src={project.image}
                                         className='w-full h-full object-cover'
                                         alt={project.title}
                                         priority
                                     />
-                                </div>
+                                </a>
                                 <a href={project.link} className="p-6 cursor-pointer flex flex-col flex-grow">
                                     <h3 className="text-xl font-bold text-gray-800 mb-2">{project.title}</h3>
                                     {/* <p className="text-gray-600 mb-4 flex-grow">{project.description}</p> */}
